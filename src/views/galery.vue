e247b2dee1e1a4153b53c6b20db77d1ea8157f05
bay007 <tnt.galicia@gmail.com>
2018-12-15 15:23:55 -0600

    Component composed

--- src/views/galery.vue
+++ src/views/galery.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>Ésta es la galeria</h1>
+  <div>
+    <h1>Ésta es la galeria con {{getImagesLength}} elementos</h1>
+    <ul class="galery_container">
+      <li class="images_group" v-for="image in images" :key="image.id">
+        <Imagen v-bind:image_obj="image"/>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Imagen from "@/components/Image.vue";
 export default {
-  name: "galery"
+  components: { Imagen },
+  name: "galery",
+  data() {
+    return {
+      images: [
+        {
+          albumId: 1,
+          id: 1,
+          title: "accusamus beatae ad facilis cum similique qui sunt",
+          url: "https://via.placeholder.com/600/92c952",
+          thumbnailUrl: "https://via.placeholder.com/150/92c952"
+        }
+      ]
+    };
+  },
+  computed: {
+    getImagesLength() {
+      return this.images.length;
+    }
+  }
 };
 </script>
 
@@ -12,5 +39,24 @@ export default {
 <style scoped lang="stylus">
 h1 {
   color: red;
+}
+
+.galery_container {
+  justify-content: start;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.images_group {
+  font-weight: bold;
+  font-size: 3em;
+  text-align: center;
 }
 </style>
