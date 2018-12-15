0f87446bb25faf128973771c20a7db562d338dbb
bay007 <tnt.galicia@gmail.com>
2018-12-14 22:38:40 -0600

    Add galery view

---
+++ src/views/galery.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Ésta es la galeria</h1>
+</template>
+
+<script>
+export default {
+  name: "galery"
+};
+</script>
+
+
+<style scoped lang="stylus">
+h1 {
+  color: red;
+}
+</style>
