4144736244fde19ba948f12e09dfde443c8e0bdb
bay007 <tnt.galicia@gmail.com>
2019-01-02 02:43:35 -0600

    Add Login Component

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import Galery from "./views/Galery.vue"
+import Home from "@/views/Home.vue";
+import Galery from "@/views/Galery.vue";
+import Login from "@/views/Login.vue";
+
 Vue.use(Router);
 
 export default new Router({
@@ -17,12 +19,17 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import( /* webpackChunkName: "about" */ "./views/About.vue")
+        import( /* webpackChunkName: "about" */ "@/views/About.vue")
     },
     {
       path: "/galery",
       name: "galery",
       component: Galery
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 });
